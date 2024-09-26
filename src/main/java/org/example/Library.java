e7cfe62025d8147ed66ad2fab0d7320d56e6a1da
SaiShelar1234 <saisanjayshelar@gmail.com>
2024-09-27 04:37:16 +0530

    Method to add a book to the library

--- src/main/java/org/example/Library.java
+++ src/main/java/org/example/Library.java
@@ -1,4 +1,20 @@
 package org.example;
-
+import java.util.ArrayList;
+import java.util.List;
 public class Library {
+    private List<Book> books;
+
+    // Constructor to initialize Library with an empty list of books
+    public Library() {
+        books = new ArrayList<>();
+    }
+
+    // Method to add a book to the library
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    public int getBooks() {
+        return books.size();
+    }
 }
