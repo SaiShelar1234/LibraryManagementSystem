5a8b845d3706c6bd6254a103f62496387b170f26
SaiShelar1234 <saisanjayshelar@gmail.com>
2024-09-27 04:40:46 +0530

    Method to check if a book is available based on its ISBN

--- src/main/java/org/example/Library.java
+++ src/main/java/org/example/Library.java
@@ -17,4 +17,18 @@ public class Library {
     public int getBooks() {
         return books.size();
     }
+
+    // Method to check if a book is available based on its ISBN
+    public boolean isBookAvailable(String ISBN) throws Exception {
+        for (Book book : books) {
+            if (book.getISBN().equals(ISBN)) {
+                if (book.isAvailable()) {
+                    return true;
+                } else {
+                    throw new Exception("Book is Not Available");
+                }
+            }
+        }
+        throw new Exception("Book with ISBN " + ISBN + " does not exist in the library.");
+    }
 }
